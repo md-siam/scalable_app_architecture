5b2c0d36daf654dabca8a429914f4f3f34ac8dc0
md-siam <md.siam03@gmail.com>
2022-07-23 09:40:08 +0600

    minor bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             const Text(
               'You have pushed the button this many times:',
             ),
